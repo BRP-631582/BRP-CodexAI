7344b5662d9256635566dae4631c256cb528a5a5
LokosFero <bilardoraka64@gmail.com>
2023-03-20 20:16:41 +0700

    crash Course

--- server/server.js
+++ server/server.js
@@ -1,50 +1,50 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
-dotenv.config()
+dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-        message: 'Hello Para Manusia Susah',
-    })
-})
-
-app.post('/', async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
-
-        const response = await openai.createCompletion({
-            model:"text-davinci-003",
-            prompt:`${prompt}`,
-            temperature:0,
-            max_tokens:3000,
-            top_p:1,
-            frequency_penalty:0.5,
-            presence_penalty:0,
-        });
-
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error || 'Terjadi Kesalahan.')
-    }
-})
-
-const server = app.listen(process.env.PORT || 5000, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello Para Manusia Susah",
   });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Terjadi Kesalahan.");
+  }
+});
+
+const server = app.listen(process.env.PORT || 8080, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
